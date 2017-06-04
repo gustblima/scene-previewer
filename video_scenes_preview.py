c4576a7a4112802deb50fd836654ddbf6b80d9f2
gustblima <gustavo.b.lima@hotmail.com>
2017-06-04 17:10:35 -0300

    updated arguments

--- video_scenes_preview.py
+++ video_scenes_preview.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys, os
 import imageio
 import shutil
-
+import argparse
 
 # mean squared error
 def calculate_difference_mse(im1, im2):
@@ -35,26 +35,28 @@ def transform_to_gif(duration, output):
 
 
 def main():
-    if len(sys.argv) < 5:
-        print "Not enough parameters"
-        exit(0)
 
-    # video path
-    video_path = str(sys.argv[1])
 
+    parser =  argparse.ArgumentParser(description='Detect scene transition.')
+    parser.add_argument("-f", "--file", help="Input File")
+    parser.add_argument("-m", "--mode", help="Select mode to compare frames, choose 'mse' or 'simple' ")
+    parser.add_argument("-t", "--threshold", type=float, help="Minimum threshold to compare frames")
+    parser.add_argument("-d", "--duration", type=float, help="Frame gif duration in seconds")
+    args = parser.parse_args()
+    video_path = args.file
     if video_path is None:
         print "You need to specify the video path"
         exit(0)
+
+
 
     # video information
     video_name = video_path.split('.')[0]
     video_capture = cv2.VideoCapture(video_path)
     total_frames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
 
-    # mode
-    mode = str(sys.argv[2])
-
     # mode selection
+    mode = args.mode if args.mode is not None else 'simple'
     if mode == 'simple':
         max_error = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH) * video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT) * 255
     elif mode == 'mse':
@@ -65,13 +67,10 @@ def main():
         exit(0)
 
     # threshold
-    threshold = float(sys.argv[3])
+    threshold =  args.threshold if args.threshold is not None else 0.2
     if threshold < 0 or threshold > 1:
         print "Wrong threshold selected."
         exit(0)
-
-    # gif duration
-    duration = float(sys.argv[4])
 
     # working directory
     if os.path.exists("temp"):
@@ -98,14 +97,15 @@ def main():
             diff_ratio = calculate_difference(current_frame, previous_frame, mode) / max_error
             if diff_ratio > threshold:
                 captured_frames += 1
-                file_name = "temp\%s_%d_%d.png" %( video_name, captured_frames, frame_index)
+                file_name = "temp\%s_%s_%d.png" %( video_name, str(captured_frames).zfill(4), frame_index)
+                print "Frame %d detected" % frame_index
                 cv2.imwrite(file_name, frame)
         previous_frame = current_frame
         frame_index += 1
     video_capture.release()
     print "Captured Scene Changes: %d" %captured_frames
+    duration = args.duration if args.duration is not None else 0.5
     transform_to_gif(duration, video_name)
-    shutil.rmtree("temp")
 
 
 if __name__ == "__main__":
